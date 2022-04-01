eb1461428dbaa7652e7fee743138b73f757a493d
Lakisha <lazar.cve@gmail.com>
2022-04-01 10:09:58 +0200

    nesto

--- BigBrainTimeSite/index.php
+++ BigBrainTimeSite/index.php
@@ -1,5 +1,9 @@
 <?php include "./php/inc/header.php"; ?>
     <h1>Dobrodosli na sajt</h1>
+
+
+
+    
     <?php 
         if (isset($_SESSION['email'])) {?>
             <img src="data:image/jpg;charset=utf8;base64,<?php $slika = vratiSliku(); echo base64_encode($slika); ?>" /> 
